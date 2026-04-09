5fe814311af28ccdb6ab3a01c179dd12cb31bba2
FarabyMalau <farabymuzakki49@gmail.com>
2026-04-09 16:41:06 +0700

    Tambahkan konstruktor dan Form1_Load

--- CRUDMahasiswaADO/Form1.cs
+++ CRUDMahasiswaADO/Form1.cs
@@ -2,6 +2,33 @@ using System;
 using System.Data.SqlClient;
 using System.Windows.Forms;
 
-private readonly SqlConnection conn;
-private readonly string connectionString =
-    "Data Source=DZAKNERZ\\DATABASEABY;Initial Catalog=DBAkademikADO;Integrated Security=True";
+namespace CRUDMahasiswaADO
+{
+    public partial class FormMahasiswa : Form
+    {
+        private readonly SqlConnection conn;
+        private readonly string connectionString =
+            "Data Source=DZAKNERZ\\DATABASEABY;Initial Catalog=DBAkademikADO;Integrated Security=True";
+
+        public FormMahasiswa()
+        {
+            InitializeComponent();
+            conn = new SqlConnection(connectionString);
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            cmbkJK.Items.Clear();
+            cmbkJK.Items.Add("L");
+            cmbkJK.Items.Add("P");
+
+            dataGridView1.SelectionMode = DataGridViewSelectionMode.FullRowSelect;
+            dataGridView1.MultiSelect = false;
+            dataGridView1.ReadOnly = true;
+            dataGridView1.AllowUserToAddRows = false;
+            dataGridView1.AutoSizeColumnsMode = DataGridViewAutoSizeColumnsMode.Fill;
+
+            dataGridView1.CellClick += dataGridView1_CellClick;
+        }
+    }
+}
