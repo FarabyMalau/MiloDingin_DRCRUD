3e1809dc4af1a252a906ad0e75023a65bd5e3c0d
FarabyMalau <farabymuzakki49@gmail.com>
2026-06-20 06:46:27 +0700

    Menambahkan data datble

--- CRUDMahasiswaADO/DAL.cs
+++ CRUDMahasiswaADO/DAL.cs
@@ -187,3 +187,69 @@ public void InsertLog(string message)
     cmd.ExecuteNonQuery();
 }
 
+public DataTable getProdi()
+{
+    if (conn.State == ConnectionState.Closed)
+    {
+        conn.Open();
+    }
+
+    SqlCommand cmd = new SqlCommand("select namaprodi from prodi", conn);
+    cmd.CommandType = CommandType.Text;
+    dtProdi = new DataTable();
+    da = new SqlDataAdapter(cmd);
+    da.Fill(dtProdi);
+
+    return dtProdi;
+}
+
+2 references
+public DataTable getDataRekap(string prodi, DateTime tanggalMasuk)
+{
+    if (conn.State == ConnectionState.Closed)
+    {
+        conn.Open();
+    }
+
+    SqlCommand cmd = new SqlCommand("sp_Report", conn);
+    cmd.CommandType = CommandType.StoredProcedure;
+    cmd.Parameters.AddWithValue("@inProdi", prodi);
+    cmd.Parameters.AddWithValue("@inTglMsuk", tanggalMasuk.Year.ToString());
+
+    da = new SqlDataAdapter(cmd);
+
+    dtMahasiswa = new DataTable();
+    da.Fill(dtMahasiswa);
+    return dtMahasiswa;
+}
+
+public DataTable getAllDataChart()
+{
+    if (conn.State == ConnectionState.Closed)
+    {
+        conn.Open();
+    }
+    SqlCommand cmd = new SqlCommand("sp_DashBoard", conn);
+    cmd.CommandType = CommandType.StoredProcedure;
+    da = new SqlDataAdapter(cmd);
+    dtMahasiswa = new DataTable();
+    da.Fill(dtMahasiswa);
+    return dtMahasiswa;
+}
+
+1 reference
+public DataTable getDataChartByTahun(DateTime thMasuk)
+{
+    if (conn.State == ConnectionState.Closed)
+    {
+        conn.Open();
+    }
+
+    SqlCommand cmd = new SqlCommand("sp_DashBoardByTahun", conn);
+    cmd.CommandType = CommandType.StoredProcedure;
+    cmd.Parameters.AddWithValue("@inTglMsuk", thMasuk.Year);
+    da = new SqlDataAdapter(cmd);
+    dtMahasiswa = new DataTable();
+    da.Fill(dtMahasiswa);
+    return dtMahasiswa;
+}
